f1dd8cf2e026b7550266c1ff2d5d1a5a6fc63cd3
Veerendranath <veerendranath0312@gmail.com>
2025-02-25 18:30:29 -0500

    Create SignIn.jsx again

---
+++ client/src/pages/SignIn/SignIn.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+import AuthForm from "../../components/AuthForm";
+import FormGroup from "../../components/FormGroup";
+import JoinUs from "../../components/JoinUs";
+import Footer from "../../components/Footer";
+
+import "./signin.css";
+import astroImage from "../../assets/Astro.svg";
+
+function SignIn() {
+  return (
+    <div className="signin">
+      <Navbar />
+      <section className="signin__hero">
+        <div className="container">
+          <AuthForm
+            title="Sign in"
+            btnText="Sign in with email"
+            alternateBtnText="Sign in with Microsoft"
+            isSignIn={true}
+          >
+            <FormGroup
+              label="Email"
+              input="email"
+              name="email"
+              placeholderText="Email address"
+            />
+          </AuthForm>
+          <div className="signin__illustration">
+            <img src={astroImage} alt="" />
+          </div>
+
+          <JoinUs />
+          <Footer />
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default SignIn;
