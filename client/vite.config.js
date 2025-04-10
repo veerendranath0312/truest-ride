b09b3370df44de6227cf94a4df16f21a9bd60ffb
Veerendranath <veerendranath0312@gmail.com>
2025-04-10 00:09:47 -0400

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig((command, mode) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-  server: {
-    proxy: {
-      // "/socket.io": {
-      //   target: "http://127.0.0.1:5000",
-      //   ws: true,
-      //   changeOrigin: true,
-      // },
-      "/api": {
-        target: "http://127.0.0.1:5000/", // Backend server
-        changeOrigin: true, // Change the origin of the request to match the target
+  return {
+    plugins: [react()],
+
+    server: {
+      proxy: {
+        // "/socket.io": {
+        //   target: "http://127.0.0.1:5000",
+        //   ws: true,
+        //   changeOrigin: true,
+        // },
+        "/api": {
+          target: env.VITE_CORS_ORIGINS, // Backend server
+          changeOrigin: true, // Change the origin of the request to match the target
+        },
+        allowedHosts: ["cassini.cs.kent.edu"],
       },
     },
-  },
+  };
 });
