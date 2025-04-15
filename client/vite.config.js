65d89ce4a76a0276d3b7f6754328dc1ec0ad0229
Veerendranath <veerendranath0312@gmail.com>
2025-04-15 18:57:48 -0400

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,17 +10,12 @@ export default defineConfig((command, mode) => {
 
     server: {
       proxy: {
-        // "/socket.io": {
-        //   target: "http://127.0.0.1:5000",
-        //   ws: true,
-        //   changeOrigin: true,
-        // },
         "/api": {
           target: env.VITE_CORS_ORIGINS, // Backend server
           changeOrigin: true, // Change the origin of the request to match the target
         },
-        allowedHosts: ["truestride.kentcs.org", "cassini.cs.kent.edu"],
       },
+      allowedHosts: ["truestride.kentcs.org", "cassini.cs.kent.edu"],
     },
   };
 });
