cbac41decca580becc0c0d3fd4eb5845d551fef8
Veerendranath <veerendranath0312@gmail.com>
2025-04-20 18:55:00 -0400

    Implement functionality to show riders profile

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -37,6 +37,7 @@ createRoot(document.getElementById("root")).render(
             <Route path="chats/:chatId" element={<Chats />} />
             <Route path="library" element={<Library />} />
             <Route path="profile" element={<Profile />} />
+            <Route path="user/:username" element={<Profile />} />
           </Route>
         </Route>
       </Routes>
